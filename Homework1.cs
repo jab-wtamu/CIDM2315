aa661b63cf5d4e3f3ead8ad71bdc1e2272a81ac4
jab-wtamu <jabarrios2@buffs.wtamu.edu>
2025-01-20 12:52:07 +0100

    Update Homework1.cs

--- Homework1.cs
+++ Homework1.cs
@@ -5,10 +5,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        //initialize variables
         double X = 2.5;
         double Y = 3.3;
         double Z = 4*X*X +3*Y;
-           
+        // display answer   
         Console.WriteLine($"X = {X}, Y = {Y}");
         Console.WriteLine($"The value of Z is: {Z}");
 
